f9ef13f76e243d84776a5fd7271bdbe740a6cc9a
DiegoElRey <d.dimitrov@bpmhub.com>
2023-11-12 00:23:25 +0200

    netlify error-9

--- src/hooks/useCalculateResult.tsx
+++ src/hooks/useCalculateResult.tsx
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Button } from '../features/button/buttonSlice'
 
 //! custom hook to calculate the sum and decide if you win
 //! for now only odd and even numbers
-export const useCalculateResult: React.FC<Button[]> = (
-	currentSelectedNumbers: Button[]
-) => {
-	const [result, setResult] = useState(0)
+export const useCalculateResult = (currentSelectedNumbers: Button[]) => {
+	const [state, setResult] = useState(0)
 
 	useEffect(() => {
 		if (currentSelectedNumbers.length === 0) return
@@ -20,7 +18,7 @@ export const useCalculateResult: React.FC<Button[]> = (
 		setResult(total)
 	}, [currentSelectedNumbers])
 
-	const judgement = result % 2 === 0 ? result : 0
+	const result = state % 2 === 0 ? state : 0
 
-	return judgement
+	return result
 }
